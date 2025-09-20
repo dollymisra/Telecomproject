2cc08512fbf80a1cbc7e9c2754fa200200ee9f95
dollymisra <63750606+dollymisra@users.noreply.github.com>
2025-09-20 19:06:49 +0530

    Delete logger.py

--- logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-def get_logger(name=__name__):
-    logger = logging.getLogger(name)
-    if not logger.hasHandlers():
-        logger.setLevel(logging.INFO)
-        ch = logging.StreamHandler()
-        ch.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-        logger.addHandler(ch)
-    return logger
